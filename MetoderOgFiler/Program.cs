aa3f116593ec275a15e6f42d4d523ff8efc079ba
Driton Latifi <drit0046@zbc.dk>
2021-01-29 10:08:49 +0100

    Metoder & Filer Program

---
+++ MetoderOgFiler/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetoderOgFiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave 1 – skriv til fil
+            File.WriteAllText(@".\StarWars.txt", "Han skød først");
+
+            // Opgave 2 – læs en tekstfil
+            string starwars = File.ReadAllText(@".\StarWars.txt");
+            Console.WriteLine(starwars);
+
+            // Opgave 3 – slet en fil
+            File.Delete(@".\StarWars.txt");
+
+            // Opgave 4 – opret en mappe
+            Directory.CreateDirectory(@".\Droids");
+
+            // Opgave 5 – slet en mappe
+            File.WriteAllText(@".\Droids\R2D2.txt", "beep bop");
+            // Directory.Delete(@".\Droids", true);
+
+            // Opgave 6 – Enumerate filer i en mappe
+            string[] files = Directory.GetFiles(@".\Droids", "*.txt", SearchOption.AllDirectories);
+            foreach (string fil in files)
+            {
+                Console.WriteLine(fil);
+            }
+
+            // Opgave 5 – læs fra fil
+            using (var reader = new StreamReader(new FileStream(@".\Droids\R2D2.txt", FileMode.Open)))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+
+            // Opgave 6 – skriv til fil
+            using (var writer = new StreamWriter(new FileStream(@".\Droids\R2D2.txt", FileMode.Create)))
+            {
+                writer.Write("Donkey Kong!");
+            }
+
+            // Opgave 7 – Fil og mappe manipulation
+            Console.Clear();
+
+            Console.WriteLine(" -Program Begyndelse-");
+
+            File.WriteAllText(@".\Sandsynlighed", "1/10"); // Add file (1)
+            File.WriteAllText(@".\Sandsynlighed2", "2/10");
+            File.WriteAllText(@".\Sandsynlighed3", "3/10");
+
+
+            File.Delete(@".\Sandsynlighed"); // Delete file (2)
+
+            string[] txtFiles = Directory.GetFiles(@"C: \Users\drito\source\repos\MetoderOgFiler\MetoderOgFiler\bin\Debug", "*.txt", SearchOption.AllDirectories); // Display files (3)
+            foreach (string file in txtFiles)
+            {
+                Console.WriteLine(file);
+            }
+
+            Directory.CreateDirectory(@".\Dansk"); // Add folder (4)
+
+            string[] txtFilesInFolder = Directory.GetFiles(@".\Droids", "*.txt", SearchOption.AllDirectories); // Display files in folder (5) 
+            foreach (string file in txtFilesInFolder)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MetoderOgFiler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MetoderOgFiler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MetoderOgFiler")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6d6cde93-8124-43e3-8c55-5b12cb1a4f89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
